5efc24c5063f18cc061eb74c994d287c2de66f36
Edi <146049944+edielestudiante2023@users.noreply.github.com>
2024-10-02 20:39:14 -0500

    proyecto rescatado

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'https://gestor.cycloidtalent.com/public/';
+    public string $baseURL = 'http://localhost/enterprise-sst202408/public/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
