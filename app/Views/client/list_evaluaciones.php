fb2d5454c3768e65cc73abcac426d0cf976741ff
p-horizontal <sistemasdegestionpropiedadhori@gmail.com>
2024-10-30 19:34:43 -0500

    tabla estandates

--- app/Views/client/list_evaluaciones.php
+++ app/Views/client/list_evaluaciones.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="es">
 
@@ -12,18 +11,34 @@
     <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
-<style>
-    /* Estilo para asegurar altura uniforme de filas */
-    .styled-table tbody tr {
-        height: 50px; /* Ajusta la altura según lo necesites */
-        white-space: nowrap;
-        overflow: hidden;
-        text-overflow: ellipsis;
-    }
-    .styled-table tbody tr td {
-        vertical-align: middle; /* Centra verticalmente el contenido */
-    }
-</style>
+    <style>
+        /* Estilo para asegurar altura uniforme de filas */
+        .styled-table {
+            table-layout: fixed;
+            /* Distribuye el ancho de las columnas equitativamente */
+            width: 100%;
+        }
+
+        .styled-table thead th,
+        .styled-table tbody td {
+            width: 1%;
+            /* Fuerza que cada columna tome el mismo ancho */
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
+        }
+
+        .styled-table tbody tr {
+            height: 50px;
+            /* Ajusta la altura según lo necesites */
+        }
+
+        .styled-table tbody tr td {
+            vertical-align: middle;
+            /* Centra verticalmente el contenido */
+        }
+    </style>
+
 </head>
 
 <body class="bg-light">
@@ -126,22 +141,22 @@
         </div>
 
         <div>
-            <table class="styled-table table table-striped">
+            <table id="evaluacionesTable" class="styled-table table table-striped">
                 <thead>
                     <tr>
-                        <th>ID</th>
+                        <!-- <th>ID</th>
                         <th>Fecha Creación</th>
-                        <th>Fecha Actualización</th>
+                        <th>Fecha Actualización</th> -->
                         <th>Cliente</th>
                         <th>Ciclo</th>
                         <th>Estándar</th>
                         <th>Detalle Estándar</th>
-                        <th>Estándares Mínimos</th>
-                        <th>Numeral</th>
-                        <th>Numerales del Cliente</th>
+                        <!-- <th>Estándares Mínimos</th> -->
+                        <!-- <th>Numeral</th> -->
+                        <!-- <th>Numerales del Cliente</th>
                         <th>Siete</th>
                         <th>Veintiún</th>
-                        <th>Sesenta</th>
+                        <th>Sesenta</th> -->
                         <th>Item del Estándar</th>
                         <th>Evaluación Inicial</th>
                         <th>Valor</th>
@@ -149,9 +164,41 @@
                         <th>Item</th>
                         <th>Criterio</th>
                         <th>Modo de Verificación</th>
-                        <th>Calificación</th>
-                        <th>Nivel de Evaluación</th>
+                        <!-- <th>Calificación</th>
+                        <th>Nivel de Evaluación</th> -->
                         <th>Observaciones</th>
+                    </tr>
+                    <tr>
+                        <!-- Filtros de búsqueda para cada columna relevante -->
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th><select id="filterCiclo" class="form-select">
+                                <option value="">Ciclo</option>
+                            </select></th>
+                        <th><select id="filterEstandar" class="form-select">
+                                <option value="">Estándar</option>
+                            </select></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th><select id="filterEvaluacionInicial" class="form-select">
+                                <option value="">Evaluación Inicial</option>
+                            </select></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
@@ -162,19 +209,19 @@
                     <?php else: ?>
                         <?php foreach ($evaluaciones as $evaluacion): ?>
                             <tr>
-                                <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['id_ev_ini']) ?>"><?= esc($evaluacion['id_ev_ini']) ?></td>
+                                <!-- <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['id_ev_ini']) ?>"><?= esc($evaluacion['id_ev_ini']) ?></td>
                                 <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['created_at']) ?>"><?= esc($evaluacion['created_at']) ?></td>
-                                <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['updated_at']) ?>"><?= esc($evaluacion['updated_at']) ?></td>
+                                <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['updated_at']) ?>"><?= esc($evaluacion['updated_at']) ?></td> -->
                                 <td data-bs-toggle="tooltip" title="<?= esc($client['nombre_cliente']) ?>"><?= esc($client['nombre_cliente']) ?></td>
                                 <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['ciclo']) ?>"><?= esc($evaluacion['ciclo']) ?></td>
                                 <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['estandar']) ?>"><?= esc($evaluacion['estandar']) ?></td>
                                 <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['detalle_estandar']) ?>"><?= esc($evaluacion['detalle_estandar']) ?></td>
-                                <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['estandares_minimos']) ?>"><?= esc($evaluacion['estandares_minimos']) ?></td>
-                                <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['numeral']) ?>"><?= esc($evaluacion['numeral']) ?></td>
-                                <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['numerales_del_cliente']) ?>"><?= esc($evaluacion['numerales_del_cliente']) ?></td>
+                                <!-- <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['estandares_minimos']) ?>"><?= esc($evaluacion['estandares_minimos']) ?></td> -->
+                                <!-- <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['numeral']) ?>"><?= esc($evaluacion['numeral']) ?></td> -->
+                                <!-- <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['numerales_del_cliente']) ?>"><?= esc($evaluacion['numerales_del_cliente']) ?></td>
                                 <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['siete']) ?>"><?= esc($evaluacion['siete']) ?></td>
                                 <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['veintiun']) ?>"><?= esc($evaluacion['veintiun']) ?></td>
-                                <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['sesenta']) ?>"><?= esc($evaluacion['sesenta']) ?></td>
+                                <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['sesenta']) ?>"><?= esc($evaluacion['sesenta']) ?></td> -->
                                 <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['item_del_estandar']) ?>"><?= esc($evaluacion['item_del_estandar']) ?></td>
                                 <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['evaluacion_inicial']) ?>"><?= esc($evaluacion['evaluacion_inicial']) ?></td>
                                 <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['valor']) ?>"><?= esc($evaluacion['valor']) ?></td>
@@ -182,29 +229,54 @@
                                 <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['item']) ?>"><?= esc($evaluacion['item']) ?></td>
                                 <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['criterio']) ?>"><?= esc($evaluacion['criterio']) ?></td>
                                 <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['modo_de_verificacion']) ?>"><?= esc($evaluacion['modo_de_verificacion']) ?></td>
-                                <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['calificacion']) ?>"><?= esc($evaluacion['calificacion']) ?></td>
-                                <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['nivel_de_evaluacion']) ?>"><?= esc($evaluacion['nivel_de_evaluacion']) ?></td>
+                                <!-- <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['calificacion']) ?>"><?= esc($evaluacion['calificacion']) ?></td>
+                                <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['nivel_de_evaluacion']) ?>"><?= esc($evaluacion['nivel_de_evaluacion']) ?></td> -->
                                 <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['observaciones']) ?>"><?= esc($evaluacion['observaciones']) ?></td>
                             </tr>
                         <?php endforeach; ?>
                     <?php endif; ?>
                 </tbody>
             </table>
-
-            <!-- Incluir Bootstrap JavaScript y script para inicializar tooltips -->
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-            <script>
-                document.addEventListener('DOMContentLoaded', function() {
-                    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-                    var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
-                        return new bootstrap.Tooltip(tooltipTriggerEl);
-                    });
-                });
-            </script>
         </div>
 
 
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            // Inicializar DataTable con búsqueda global y selectores de filtros
+            var table = $('#evaluacionesTable').DataTable({
+                initComplete: function() {
+                    var api = this.api();
+
+                    // Añadir opciones únicas a cada filtro por columna
+                    api.columns([4, 5, 14]).every(function() {
+                        var column = this;
+                        var select = $(column.footer()).find('select');
+
+                        column.data().unique().sort().each(function(d) {
+                            select.append('<option value="' + d + '">' + d + '</option>');
+                        });
+
+                        select.on('change', function() {
+                            var val = $.fn.dataTable.util.escapeRegex($(this).val());
+                            column.search(val ? '^' + val + '$' : '', true, false).draw();
+                        });
+                    });
+                }
+            });
+
+            // Inicializar tooltips de Bootstrap
+            var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+            var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+                return new bootstrap.Tooltip(tooltipTriggerEl);
+            });
+        });
+    </script>
 
 </body>
 
