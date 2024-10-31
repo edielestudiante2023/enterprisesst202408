29f9058420cbaae38bc4e72105777cc799556894
p-horizontal <sistemasdegestionpropiedadhori@gmail.com>
2024-10-30 19:51:11 -0500

    ajustando tabla

--- app/Views/client/list_evaluaciones.php
+++ app/Views/client/list_evaluaciones.php
@@ -8,21 +8,19 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
 
     <style>
-        /* Estilo para asegurar altura uniforme de filas */
+        /* Configurar la tabla con 80% de ancho y ancho fijo de columnas */
         .styled-table {
             table-layout: fixed;
-            /* Distribuye el ancho de las columnas equitativamente */
-            width: 100%;
+            width: 80%;
+            /* Ancho total de la tabla */
         }
 
         .styled-table thead th,
         .styled-table tbody td {
-            width: 40%;
-            /* Fuerza que cada columna tome el mismo ancho */
             white-space: nowrap;
             overflow: hidden;
             text-overflow: ellipsis;
@@ -30,12 +28,6 @@
 
         .styled-table tbody tr {
             height: 50px;
-            /* Ajusta la altura según lo necesites */
-        }
-
-        .styled-table tbody tr td {
-            vertical-align: middle;
-            /* Centra verticalmente el contenido */
         }
     </style>
 
@@ -144,19 +136,11 @@
             <table id="evaluacionesTable" class="styled-table table table-striped">
                 <thead>
                     <tr>
-                        <!-- <th>ID</th>
-                        <th>Fecha Creación</th>
-                        <th>Fecha Actualización</th> -->
+                        <!-- Ajuste a columnas necesarias y activación de tooltips -->
                         <th>Cliente</th>
                         <th>Ciclo</th>
                         <th>Estándar</th>
                         <th>Detalle Estándar</th>
-                        <!-- <th>Estándares Mínimos</th> -->
-                        <!-- <th>Numeral</th> -->
-                        <!-- <th>Numerales del Cliente</th>
-                        <th>Siete</th>
-                        <th>Veintiún</th>
-                        <th>Sesenta</th> -->
                         <th>Item del Estándar</th>
                         <th>Evaluación Inicial</th>
                         <th>Valor</th>
@@ -164,15 +148,10 @@
                         <th>Item</th>
                         <th>Criterio</th>
                         <th>Modo de Verificación</th>
-                        <!-- <th>Calificación</th>
-                        <th>Nivel de Evaluación</th> -->
                         <th>Observaciones</th>
                     </tr>
                     <tr>
-                        <!-- Filtros de búsqueda para cada columna relevante -->
-                        <th></th>
-                        <th></th>
-                        <th></th>
+                        <!-- Filtros para columnas relevantes: Cliente, Ciclo, Estándar, Evaluación Inicial -->
                         <th></th>
                         <th><select id="filterCiclo" class="form-select">
                                 <option value="">Ciclo</option>
@@ -180,12 +159,6 @@
                         <th><select id="filterEstandar" class="form-select">
                                 <option value="">Estándar</option>
                             </select></th>
-                        <th></th>
-                        <th></th>
-                        <th></th>
-                        <th></th>
-                        <th></th>
-                        <th></th>
                         <th></th>
                         <th></th>
                         <th><select id="filterEvaluacionInicial" class="form-select">
@@ -197,31 +170,20 @@
                         <th></th>
                         <th></th>
                         <th></th>
-                        <th></th>
-                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
                     <?php if (empty($evaluaciones)): ?>
                         <tr>
-                            <td colspan="23">No hay evaluaciones para este cliente.</td>
+                            <td colspan="12">No hay evaluaciones para este cliente.</td>
                         </tr>
                     <?php else: ?>
                         <?php foreach ($evaluaciones as $evaluacion): ?>
                             <tr>
-                                <!-- <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['id_ev_ini']) ?>"><?= esc($evaluacion['id_ev_ini']) ?></td>
-                                <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['created_at']) ?>"><?= esc($evaluacion['created_at']) ?></td>
-                                <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['updated_at']) ?>"><?= esc($evaluacion['updated_at']) ?></td> -->
                                 <td data-bs-toggle="tooltip" title="<?= esc($client['nombre_cliente']) ?>"><?= esc($client['nombre_cliente']) ?></td>
                                 <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['ciclo']) ?>"><?= esc($evaluacion['ciclo']) ?></td>
                                 <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['estandar']) ?>"><?= esc($evaluacion['estandar']) ?></td>
                                 <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['detalle_estandar']) ?>"><?= esc($evaluacion['detalle_estandar']) ?></td>
-                                <!-- <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['estandares_minimos']) ?>"><?= esc($evaluacion['estandares_minimos']) ?></td> -->
-                                <!-- <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['numeral']) ?>"><?= esc($evaluacion['numeral']) ?></td> -->
-                                <!-- <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['numerales_del_cliente']) ?>"><?= esc($evaluacion['numerales_del_cliente']) ?></td>
-                                <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['siete']) ?>"><?= esc($evaluacion['siete']) ?></td>
-                                <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['veintiun']) ?>"><?= esc($evaluacion['veintiun']) ?></td>
-                                <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['sesenta']) ?>"><?= esc($evaluacion['sesenta']) ?></td> -->
                                 <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['item_del_estandar']) ?>"><?= esc($evaluacion['item_del_estandar']) ?></td>
                                 <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['evaluacion_inicial']) ?>"><?= esc($evaluacion['evaluacion_inicial']) ?></td>
                                 <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['valor']) ?>"><?= esc($evaluacion['valor']) ?></td>
@@ -229,14 +191,13 @@
                                 <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['item']) ?>"><?= esc($evaluacion['item']) ?></td>
                                 <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['criterio']) ?>"><?= esc($evaluacion['criterio']) ?></td>
                                 <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['modo_de_verificacion']) ?>"><?= esc($evaluacion['modo_de_verificacion']) ?></td>
-                                <!-- <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['calificacion']) ?>"><?= esc($evaluacion['calificacion']) ?></td>
-                                <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['nivel_de_evaluacion']) ?>"><?= esc($evaluacion['nivel_de_evaluacion']) ?></td> -->
                                 <td data-bs-toggle="tooltip" title="<?= esc($evaluacion['observaciones']) ?>"><?= esc($evaluacion['observaciones']) ?></td>
                             </tr>
                         <?php endforeach; ?>
                     <?php endif; ?>
                 </tbody>
             </table>
+
         </div>
 
 
@@ -248,15 +209,15 @@
 
     <script>
         $(document).ready(function() {
-            // Inicializar DataTable con búsqueda global y selectores de filtros
+            // Inicializar DataTable y configuración de filtros
             var table = $('#evaluacionesTable').DataTable({
                 initComplete: function() {
                     var api = this.api();
 
-                    // Añadir opciones únicas a cada filtro por columna
-                    api.columns([4, 5, 14]).every(function() {
+                    // Filtros dinámicos para Ciclo, Estándar y Evaluación Inicial
+                    api.columns([1, 2, 5]).every(function() {
                         var column = this;
-                        var select = $(column.footer()).find('select');
+                        var select = $(column.header()).find('select');
 
                         column.data().unique().sort().each(function(d) {
                             select.append('<option value="' + d + '">' + d + '</option>');
